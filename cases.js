25263138514d45ab8f2184dde508344a6bd2356e
myusername <marianne.escubin@gmail.com>
2024-01-13 22:19:17 +0800

    Boosbot case link

--- cases.js
+++ cases.js
@@ -1,7 +1,7 @@
 export const caseStudies = [
   {
     img: "./photos/app-launch.svg",
-    link: "mariannetrizha.github.io/cv",
+    link: "https://shade-leo-dfe.notion.site/How-I-revamped-the-customer-journey-and-tripled-paying-users-at-BoostBot-through-AI-b49d5f14244140bba8a702a705916315?pvs=4",
     title: "Launched this AI tool and 3x-d paying users",
     subtitle: `Built a first of its kind AI powered search tool for influencer
         marketing in six weeks. Another six weeks later, MAU 3x-d.`,
@@ -9,8 +9,7 @@ export const caseStudies = [
   {
     img: "./photos/img3.svg",
     link: "mariannetrizha.github.io/cv",
-    title:
-      "Shrunk onboarding time from weeks to mere minutes through 3rd party partnerships",
+    title: "More efficient ",
     subtitle: `A collab with deel.com's product team cut our onboarding time from
     weeks to minutes. Users were happy and our internal HR team just
     saved 50 hours.`,
