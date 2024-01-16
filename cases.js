1630442f216532ffe611179cd46def3ac8f51f25
myusername <marianne.escubin@gmail.com>
2024-01-16 21:45:57 +0800

    button changes

--- cases.js
+++ cases.js
@@ -4,22 +4,20 @@ export const caseStudies = [
     link: "https://shade-leo-dfe.notion.site/How-I-revamped-the-customer-journey-and-tripled-paying-users-at-BoostBot-through-AI-b49d5f14244140bba8a702a705916315?pvs=4",
     title:
       "How I revamped the customer journey at BoostBot and tripled paying users through AI",
-    subtitle: `Founders usually call me to get things from 0 to 1. To launch BoostBot, I used several research techniques, mapped out the customer journey, and created a roadmap. Six weeks post launch, we tripled paying users and BoostBot would set out to become the 2nd Product of the Day in Product Hunt.`,
+    subtitle: `Founders usually call me to get things from 0 to 1. To launch BoostBot, I used several research techniques, mapped out the customer journey, and created a roadmap. Six weeks post launch, we tripled paying users and BoostBot would become the 2nd Product of the Day in Product Hunt.`,
   },
   {
     img: "./photos/img3.svg",
     link: "https://shade-leo-dfe.notion.site/How-I-shortened-the-onboarding-time-at-Magic-from-weeks-to-minutes-a2fb9a9f8568483ba774eef819fbe342",
     title:
       "How I shortened the onboarding time at Magic, from weeks to minutes",
-    subtitle: `Third party service providers are usually heaven sent. But what happens when their tech are unable to keep up with your business needs?
-    This was our dillemma with Deel. Thankfully, they responded well to our product request. And a tech collab lead to a faster and more efficient onboarding.`,
+    subtitle: `Third party service providers are usually heaven-sent. But what happens when their tech are unable to keep up with your business needs?
+    This was our dillemma. Thankfully, our provider responded well to our request. And a tech and product collab lead to a faster and more efficient onboarding.`,
   },
   {
     img: "./photos/img4.svg",
     link: "https://shade-leo-dfe.notion.site/How-I-halved-payment-tickets-in-Magic-with-a-dashboard-revamp-0c8b4ea267fc488cb400e2ef256d4ba0",
     title: "How I halved payment tickets in Magic with a dashboard revamp",
-    subtitle: `User interviews and sentiment analysis of Support tickets lead to
-    one conclusion: our payment dashboards need to be clearer, more
-    visible, and transparent..`,
+    subtitle: `User interviews and sentiment analysis lead to one conclusion: we were dodgy and confusing when it comes to payment communication. After batches of iteration, we finally released a new payment dashboard and guess what? Payment support tickets went down about 50%.`,
   },
 ];
