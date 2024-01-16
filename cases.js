bb6b88f82ad290b3f1d9cd1127800d520279603c
myusername <marianne.escubin@gmail.com>
2024-01-16 15:51:34 +0800

    changes in subtitles

--- cases.js
+++ cases.js
@@ -4,20 +4,19 @@ export const caseStudies = [
     link: "https://shade-leo-dfe.notion.site/How-I-revamped-the-customer-journey-and-tripled-paying-users-at-BoostBot-through-AI-b49d5f14244140bba8a702a705916315?pvs=4",
     title:
       "How I revamped the customer journey at BoostBot and tripled paying users through AI",
-    subtitle: `Founders usually call me to get things from 0 to 1. To launch BoostBot, I used several research techniques, mapped out the customer journey, and created a roadmap. Six weeks post launch, we tripled paying users and BoostBot would become 2nd Product of the Day in Product Hunt.`,
+    subtitle: `Founders usually call me to get things from 0 to 1. To launch BoostBot, I used several research techniques, mapped out the customer journey, and created a roadmap. Six weeks post launch, we tripled paying users and BoostBot would set out to become the 2nd Product of the Day in Product Hunt.`,
   },
   {
     img: "./photos/img3.svg",
     link: "https://shade-leo-dfe.notion.site/How-I-shortened-the-onboarding-time-at-Magic-from-weeks-to-minutes-a2fb9a9f8568483ba774eef819fbe342",
     title:
       "How I shortened the onboarding time at Magic, from weeks to minutes",
-    subtitle: `A collab with deel.com's product team cut our onboarding time from
-    weeks to minutes. Users were happy and our internal HR team just
-    saved 50 hours.`,
+    subtitle: `Third party service providers are usually heaven sent. But what happens when their tech are unable to keep up with your business needs?
+    This was our dillemma with Deel. Thankfully, they responded well to our product request. And a tech collab lead to a faster and more efficient onboarding.`,
   },
   {
     img: "./photos/img4.svg",
-    link: "mariannetrizha.github.io/cv",
+    link: "https://shade-leo-dfe.notion.site/How-I-halved-payment-tickets-in-Magic-with-a-dashboard-revamp-0c8b4ea267fc488cb400e2ef256d4ba0",
     title: "How I halved payment tickets in Magic with a dashboard revamp",
     subtitle: `User interviews and sentiment analysis of Support tickets lead to
     one conclusion: our payment dashboards need to be clearer, more
